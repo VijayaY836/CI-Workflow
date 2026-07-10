72f19ebdac8e710e26c156b9a5ca70a834c32fed
Vijaya_Y <24wh1a05l2@bvrithyderabad.edu.in>
2026-07-10 17:18:57 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>CI Workflow</h1>
+      <p>This app exists to give the CI pipeline something to build, lint, and test.</p>
+    </div>
+  )
+}
+
+export default App
